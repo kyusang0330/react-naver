217c1801ec583adc993c01e9f89e678835dd3863
kyusang0330 <cholongs123@naver.com>
2024-08-21 16:42:28 +0900

    Naver

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-
+import React from 'react';
 import './App.css';
+import NaverMenu from "./NaverMenu";
+import NaverSearch from "./NaverSearch";
 
 function App() {
   return (
     <div className="App">
-      <div className="search">
-        <input type="text" placeholder="검색어를 입력하세요."/>
-          <button>검색</button>
-      </div>
-        <div className="menu">
-            <div className="mail">메일</div>
-            <div className="cafe">카페</div>
-            <div className="blog">블로그</div>
-            <div className="shopping">쇼핑</div>
-            <div className="news">뉴스</div>
-            <div className="money">증권</div>
-            <div className="house">부동산</div>
-            <div className="map">지도</div>
-            <div className="webtoon">웹툰</div>
-            <div className="chi">치지직</div>
-        </div>
+        <NaverSearch />
+        <NaverMenu />
 
     </div>
   );
