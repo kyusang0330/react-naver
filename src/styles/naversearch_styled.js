af87b3c1fd269ce8cbbc6529b5071a6049985af3
kyusang0330 <cholongs123@naver.com>
2024-08-21 16:42:09 +0900

    style-component

---
+++ src/styles/naversearch_styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const NaverSearchContainer = styled.div`
+
+    z-index: 10;
+    width: 705px;
+    border: 1px solid forestgreen;
+    border-radius: 33px;
+`;
+
+export {
+    NaverSearchContainer
+};
