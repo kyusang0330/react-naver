12bb84cf563e230746e5545820fd7a7359598734
kyusang0330 <cholongs123@naver.com>
2024-08-23 14:53:17 +0900

    component

---
+++ src/components/NaverMenu.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {NaverMenuContainer,
+} from "../styles/navermenu_styled";
+
+const NaverMenu = () => {
+
+    return (
+        <NaverMenuContainer>
+            <div>메일</div>
+            <div>카페</div>
+            <div>블로그</div>
+            <div>쇼핑</div>
+            <div>뉴스</div>
+            <div>증권</div>
+            <div>부동산</div>
+            <div>지도</div>
+            <div>웹툰</div>
+            <div>치지직</div>
+        </NaverMenuContainer>
+    )
+}
+
+export default NaverMenu;
--- src/components/NaverSearch.js
+++ src/components/NaverSearch.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import {
-    NaverSearchContainer
-} from "./styles/naversearch_styled";
+    NaverSearchContainer,
+    Main,
+} from "../styles/naversearch_styled";
 
 const NaverSearch = () => {
     return (
         <NaverSearchContainer>
-            <div>
+            <Main>
             <input type="text" placeholder="검색어를 입력하세요."/>
             <button>검색</button>
-            </div>
+            </Main>
         </NaverSearchContainer>
 
     )
