f412b372d4374bf280e2746fe22ed5b258802686
kyusang0330 <cholongs123@naver.com>
2024-08-26 17:20:38 +0900

    style-component

--- src/styles/naversearch_styled.js
+++ src/styles/naversearch_styled.js
@@ -3,18 +3,28 @@ import styled from 'styled-components';
 const NaverSearchContainer = styled.div`
     display: flex;
     justify-content: center;    
+    padding: 10px;
 `;
 
 const Main = styled.div`
     display: flex;
     justify-content: center;
-    width: 705px;
+    width: 706px;
     height: 58px;
     border: 1px solid forestgreen;
     border-radius: 33px;
 `;
 
+const NaverSearchKey = styled.input`
+    width: 480px;
+    height: 24px;
+    padding: 17px;
+    position: relative;
+    z-index: 10;
+`;
+
 export {
     NaverSearchContainer,
-    Main
+    Main,
+    NaverSearchKey,
 };
