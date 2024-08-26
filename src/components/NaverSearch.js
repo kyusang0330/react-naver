2ffc242116eb564cd6bc6772e22cdb9f72dac6ed
kyusang0330 <cholongs123@naver.com>
2024-08-26 17:20:24 +0900

    Component

--- src/components/NaverSearch.js
+++ src/components/NaverSearch.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {
     NaverSearchContainer,
     Main,
+    NaverSearchKey,
+
 } from "../styles/naversearch_styled";
 
 const NaverSearch = () => {
     return (
         <NaverSearchContainer>
             <Main>
-            <input type="text" placeholder="검색어를 입력하세요."/>
+                <a href='/'></a>
+            <NaverSearchKey type="text" placeholder="검색어를 입력하세요."/>
             <button>검색</button>
             </Main>
         </NaverSearchContainer>
