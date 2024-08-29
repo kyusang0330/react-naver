fc58464025a770bbe9815d8eb8c7bc3fe1967506
kyusang0330 <cholongs123@naver.com>
2024-08-29 15:54:15 +0900

    Component

--- src/components/NaverSearch.js
+++ src/components/NaverSearch.js
@@ -10,9 +10,13 @@ const NaverSearch = () => {
     return (
         <NaverSearchContainer>
             <Main>
-                <a href='/'></a>
-            <NaverSearchKey type="text" placeholder="검색어를 입력하세요."/>
-            <button>검색</button>
+                <a href='#'>
+                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M16.273 12.845 7.376 0H0v24h7.727V11.155L16.624 24H24V0h-7.727v12.845z"></path>
+                    </svg>
+                </a>
+                <NaverSearchKey type="text" placeholder="검색어를 입력하세요."/>
+                <button>검색</button>
             </Main>
         </NaverSearchContainer>
 
