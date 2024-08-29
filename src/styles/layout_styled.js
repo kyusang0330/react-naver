c77b503f3b2774be71d803f237ca0f7c6f2dc3cd
kyusang0330 <cholongs123@naver.com>
2024-08-29 15:54:08 +0900

    style-component

--- src/styles/layout_styled.js
+++ src/styles/layout_styled.js
@@ -3,6 +3,11 @@ import styled from "styled-components";
 const LayoutContainer = styled.div`
 `;
 
+const LayoutMiddle = styled.div`
+display: flex
+`;
+
 export {
-    LayoutContainer
+    LayoutContainer,
+    LayoutMiddle,
 }
