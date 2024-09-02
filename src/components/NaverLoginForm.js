02a8c5d12bc8ed6994fee515303bf77578b42011
kyusang0330 <cholongs123@naver.com>
2024-09-02 18:43:59 +0900

    useForm

--- src/components/NaverLoginForm.js
+++ src/components/NaverLoginForm.js
@@ -1,9 +1,13 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import {UseForm} from 'react-hook-form';
 const NaverLoginForm = () => {
-
+    const {register, handleSubmit} = useForm();
+    const [data, setData] = useState();
     return (
         <div>
+            <form>
+                <label></label>
+            </form>
              </div>
     )
 }
