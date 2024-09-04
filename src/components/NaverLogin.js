507d5a450b92152fa619a902d108fe5143bee330
kyusang0330 <cholongs123@naver.com>
2024-09-04 16:22:26 +0900

    Component

--- src/components/NaverLogin.js
+++ src/components/NaverLogin.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import {NaverLoginContainer} from "../styles/naverlogin_styled";
+
 const NaverLogin = () => {
-    return(
+    return (
         <NaverLoginContainer>
             <div>
-            <p>네이버를 더 안전하고 편리하게 이용하세요.</p>
-            <button> NAVER 로그인</button>
-            <div> 아이디 찾기</div>
-            <div> 비밀번호 찾기</div>
-            <div> 회원가입</div>
+                <p>네이버를 더 안전하고 편리하게 이용하세요.</p>
+                <button> NAVER 로그인</button>
+                <div>
+                    <div> 아이디 찾기</div>
+                    <div> 비밀번호 찾기</div>
+                    <div> 회원가입</div>
+                </div>
             </div>
         </NaverLoginContainer>
     )
--- src/components/NaverMenu.js
+++ src/components/NaverMenu.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import {NaverMenuContainer,
+import {
+    NaverMenuContainer,
+    NaverMenuItem,
 } from "../styles/navermenu_styled";
 
 const NaverMenu = () => {
 
     return (
         <NaverMenuContainer>
-            <div>메일</div>
-            <div>카페</div>
-            <div>블로그</div>
-            <div>쇼핑</div>
-            <div>뉴스</div>
-            <div>증권</div>
-            <div>부동산</div>
-            <div>지도</div>
-            <div>웹툰</div>
-            <div>치지직</div>
+            <NaverMenuItem>
+                <div>메일</div>
+                <div>카페</div>
+                <div>블로그</div>
+                <div>쇼핑</div>
+                <div>뉴스</div>
+                <div>증권</div>
+                <div>부동산</div>
+                <div>지도</div>
+                <div>웹툰</div>
+                <div>치지직</div>
+            </NaverMenuItem>
         </NaverMenuContainer>
     )
 }
