e1607c6c3b44f372cb5af503361c87a17642e37d
kyusang0330 <cholongs123@naver.com>
2024-09-06 23:31:03 +0900

    styled-component

--- src/styles/navertopmenu_styled.js
+++ src/styles/navertopmenu_styled.js
@@ -2,13 +2,14 @@ import styled from "styled-components";
 import MainImg from "../assets/images/sp_main.3e7d337e.png";
 const NaverTopMenuContainer = styled.div`
     display: flex;
+    justify-content: space-between;
+
 `;
 const NaverLeftMenu=styled.div`
     display: flex;
 `;
 const NaverRightMenu=styled.div`
     display: flex;
-    justify-content: end;
 `;
 const First = styled.div`
     display: block;
